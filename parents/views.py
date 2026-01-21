1c546d9f33c8dd25ebfca4e4abc38f21b776d7f8
Kyryl0201 <zelenkokiril2002@gmail.com>
2026-01-21 18:36:06 +0100

    Initial commit

---
+++ parents/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def parents(request):
+    return "ok"
+
+def parent_student(request, student_id):
+    return f"ok {student_id}"
+
+def students_lesson_for_parents(request, lesson_id):
+    return f"ok {lesson_id}"
