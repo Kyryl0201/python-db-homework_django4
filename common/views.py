6ab836675921fea11d66ee65e42fd2500649f969
Kyryl0201 <zelenkokiril2002@gmail.com>
2026-01-28 08:44:57 +0100

    Use Django Forms for register and login

--- common/views.py
+++ common/views.py
@@ -1,51 +1,44 @@
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout
 
+from common.Forms import RegisterForm, LoginForm
 
 
 def login(request):
     if request.method == "POST":
-        user_login = request.POST.get("login")
-        password = request.POST.get("password")
-        user = auth.authenticate(request, username=user_login, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect("/user/" + str(user.id) + "/")
-        else:
-            return render(request,"login.html", context={"error": "wrong login or password"})
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(
+                request,
+                username=form.cleaned_data["login"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                auth.login(request, user)
+                return redirect(f"/user/{user.id}/")
+            else:
+                return render(request, "login.html", {"form": form, "error": "wrong login or password"})
+        return render(request, "login.html", {"form": form})
     else:
-        return render(request,"login.html")
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
 
-from django.contrib.auth.models import User, Group
-from django.shortcuts import render, redirect
 
 
 def register(request):
     if request.method == "POST":
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        user_login = request.POST.get("login")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        role = request.POST.get("role")
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        return render(request, "register.html", {"form": form})
+    else:
+        form = RegisterForm()
+    return render(request, "register.html", {"form": form})
 
-        user = User.objects.create_user(
-            user_login,
-            email,
-            password,
-            first_name=first_name,
-            last_name=last_name
-        )
-
-        group = Group.objects.get(name=role)
-        user.groups.add(group)
-
-        return redirect("login")
-
-    return render(request, "register.html")
 
 
 
--- teacher/views.py
+++ teacher/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from common.Forms import LessonForm
+from common.models import Lesson
 from common.tools import group_required
 
 
@@ -7,10 +9,17 @@ from common.tools import group_required
 def teacher_page(request):
     return HttpResponse("ok")
 
-
 @group_required("Teacher")
 def teacher_lessons(request):
-    return HttpResponse("ok")
+    if request.method == "POST":
+        create_lesson_form = LessonForm(request.POST)
+        create_lesson_form.is_valid()
+        lesson = Lesson(teacher=request.user, **create_lesson_form.cleaned_data)
+        lesson.save()
+
+    create_lesson_form = LessonForm()
+    teacher_lessons_data = Lesson.objects.filter(teacher=request.user).all()
+    return render(request, "teacher_lessons.html", context={"form":create_lesson_form,"teacher_lessons":teacher_lessons_data})
 
 
 @group_required("Teacher")
